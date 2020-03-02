51e70fc6719699d4bb1659904a658869130d3744
James Arney <wjarney@gmail.com>
2020-03-02 19:29:07 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,9 +7,8 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-user_input = gets.strip
 
-def input_to_index = [user_input.to_i] - 1
-end
 
-move = board[input_to_index]
+input_to_index = [user_input.to_i] - 1
+
+position = board[user_input]
